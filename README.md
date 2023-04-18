6b75e37cf73b7489cf286d9a82fda8c859aa43bb
ghtoui <92294389+ghtoui@users.noreply.github.com>
2023-04-18 12:16:31 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # record_analysis
+
+## templates
+
+htmlファイルはここ
+layout.html
+を使ってテンプレート作成すると楽
+
+## static
+
+javascriptとcss, 画像ファイルはここ
+録音した音声ファイルも後々ここに保存するつもり
+
+
---
+++ web_app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, send_from_directory, jsonify, render_template, send_file
+import os
+import numpy as np
+import pandas as pd
+from web_app.vege_measure import VegeMeasureClass
+from web_app.controll_data import ControllDataClass
+
+app = Flask(__name__)
+
+# configfileの読み込み
+app.config.from_object('web_app.config')
+
+
+# iconの設定
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static/images'), 'favicon.ico', )
+
+
+from web_app import view
